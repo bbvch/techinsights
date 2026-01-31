418d3530b1a33359afdf3ae99db4751ccd2cbcc6
Shpend Kelmendi <shekel7@hotmail.com>
2026-01-31 13:08:29 +0100

    correct docusaurus configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bbvch', // Usually your GitHub org/user name.
-  projectName: 'bbvtechblog.github.io', // Usually your repo name.
+  projectName: 'coffeecache.github.io', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
