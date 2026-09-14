cd2c61409917cc3689524e7b43427311abc83618
Vanesco <vanescoboehm@bbv.ch>
2026-09-14 08:35:40 +0200

    Minor Dependency Updates, clean up of typescript issues und warnings (#72)
    * Dependency updates
    
    * fix warning about wrong tag use
    
    * typescript clean ups

--- src/components/AuthorsList/index.tsx
+++ src/components/AuthorsList/index.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import authorsData from '../../../blog/authors.json';
 import Author from '@theme/Blog/Components/Author';
+import type {AuthorWithKey} from '@docusaurus/plugin-content-blog';
+
+type AuthorRecord = {
+  name: string;
+  title: string;
+  description: string;
+  url: string;
+  image: string;
+  imageURL: string;
+  socials?: {
+    linkedin?: string;
+    github?: string;
+    sessionize?: string;
+    mastodon?: string;
+  };
+};
 
 export default function AuthorsList(): React.JSX.Element {
-  Object.keys(authorsData).map((key) => {
-    authorsData[key].url = `/authors/${key}`;
+  const authors = Object.entries(authorsData as Record<string, AuthorRecord>).map(([key, author]) => ({
+    ...author,
+    key,
+    page: {permalink: `/authors/${key}`},
+    url: `/authors/${key}`,
+  }));
 
-    if (authorsData[key].socials?.linkedin) {
-      authorsData[key].socials.linkedin = "https://www.linkedin.com/in/" + authorsData[key].socials?.linkedin;
+  authors.forEach((author) => {
+    if (author.socials?.linkedin) {
+      author.socials.linkedin = 'https://www.linkedin.com/in/' + author.socials.linkedin;
     }
-    if (authorsData[key].socials?.github) {
-      authorsData[key].socials.github = "https://github.com/" + authorsData[key].socials?.github;
+    if (author.socials?.github) {
+      author.socials.github = 'https://github.com/' + author.socials.github;
     }
   });
 
   return (
     <div className="authors-list">
-      {Object.values(authorsData).map((author) => (
-        <Author as="h2" author={author} />
+      {authors.map((author) => (
+        <Author as="h2" author={author satisfies AuthorWithKey} />
       ))}
     </div>
   );
